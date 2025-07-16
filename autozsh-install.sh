a113ba9c8c767a73fc126ee9da0c4e731d867504
Daniel Speck <daniel451@mailbox.org>
2025-07-16 19:54:20 +0200

    added linebreaks for cancellation when existing install found

--- autozsh-install.sh
+++ autozsh-install.sh
@@ -28,14 +28,14 @@ fi
 
 # check for previous oh-my-zsh installation
 if [[ -d "$HOME/.oh-my-zsh" ]]; then
-    echo "An existing oh-my-zsh installation was found at $HOME/.oh-my-zsh."
+    echo "\nAn existing oh-my-zsh installation was found at $HOME/.oh-my-zsh."
     read -rp "Do you want to re-install (y/N)? " reinstall
     reinstall=${reinstall,,}
     if [[ "$reinstall" == "y" ]]; then
         echo "Removing previous installation..."
         rm -rf "$HOME/.oh-my-zsh"
     else
-        echo "installation aborted."
+        echo "\nInstallation aborted.\n"
         exit 1
     fi
 fi
